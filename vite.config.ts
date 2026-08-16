fdd455ac6ce38b621f8c56e0ebead5f59653d97b
KD101010 <kollindavis10@gmail.com>
2026-08-15 22:54:15 -0500

    Update vite.config.ts
    Fix PantryCue GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pantrycue/',
+  base: '/PantryCue/',
   plugins: [react()],
   server: {
     host: true,
